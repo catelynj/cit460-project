accb5ee6428cce9eec3a3621fd542f0c9cf7d16e
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-25 21:21:41 -0400

    create camera.py
    Added references and setup instructions for OpenCV and Tesseract.

---
+++ camera.py
@@ -0,0 +1,13 @@
+'''
+DB18 Syntax From: https://opencv.org/blog/text-detection-and-removal-using-opencv/
+
+PiCamera Reference: 
+https://pip-assets.raspberrypi.com/categories/652-raspberry-pi-camera-module-2/documents/RP-008156-DS-2-picamera2-manual.pdf?disposition=inline
+
+Some OpenCV & Tesseract Setup:
+https://pyimagesearch.com/2018/09/17/opencv-ocr-and-text-recognition-with-tesseract/
+
+Debugging issues (of which there are many) done w/ StackExchange/Reddit/etc. and Claude as a last resort
+'''
+
+# creating these ahead of time for abstraction
