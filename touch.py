8c5a973d401e9cee2ad71379c9b568c130f53310
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-25 21:59:59 -0400

    ignore

--- touch.py
+++ touch.py
@@ -5,10 +5,9 @@ Touch Sensor Control Script
 import lgpio
 
 TOUCH_PIN = 17
-
-h = lgpio.gpiochip_open(0) # init
+h = lgpio.gpiochip_open(0)
 lgpio.gpio_claim_input(h, TOUCH_PIN)
 
-def read_touch(h, touch_pin):
-    return lgpio.gpio_read(h, touch_pin)
+def read_touch():
+    return lgpio.gpio_read(h, TOUCH_PIN)
 
