e44cde12d2bfd048c9c24a03fa053592bbd62765
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-25 21:36:54 -0400

    Refactor api.py, moving speech recog

--- api.py
+++ api.py
@@ -16,44 +16,28 @@ load_dotenv()
 
 app_id = os.getenv("APP_ID")
 
-# translate test
-q_text = "hello, goodbye"
-
-#_ = ts.preaccelerate_and_speedtest() # caching available will work on adding later
-translation = ts.translate_text(query_text=q_text, translator="google", to_language="ja")
-print(translation)
-
-# text to speech test (NOT WORKING YAY)
-#engine = pyttsx3.init()
-#engine.say("hello, world")
-#engine.runAndWait()
+def translate(text):
+	q_text = "hello, goodbye"
+	#_ = ts.preaccelerate_and_speedtest() # caching available will work on adding later
+	translation = ts.translate_text(query_text=q_text, translator="google", to_language="ja")
+	print(translation)
 
 
-# speech recog
-r = sr.Recognizer()
-with sr.Microphone() as source:
-		print("Say something")
- 	audio = r.listen(source)
+def tts():
+	#engine = pyttsx3.init()
+	#engine.say("hello, world")
+	#engine.runAndWait()
 
-# Speech Recognition Engine: Sphinx (CMU)
-try:
-	print("What you said: " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-	print("Audio not recognized")
-except sr.RequestError as e:
-	print("Sphinx error; {0}".format(e))
 
-# wolfram test
-client = wolframalpha.Client(app_id)
-query = "What is the temperature in Williamsport, PA?"
-params = {
-"appid": app_id,
-"i": query
-}
-
-response = requests.get("http://api.wolframalpha.com/v1/result", params=params)
-
-print(response.text)
+def wolfram():
+	client = wolframalpha.Client(app_id)
+	query = "What is the temperature in Williamsport, PA?"
+	params = {
+		"appid": app_id,
+		"i": query
+	}
+	response = requests.get("http://api.wolframalpha.com/v1/result", params=params)
+	print(response.text)
 
 
 
