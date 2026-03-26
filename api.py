67fe015278b0ea38da3592dc066ca7802ed3fe97
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-25 21:28:12 -0400

    Update api.py to add speech recognition
    Not tested yet, pulled from speech_recognition package github examples!

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 
 '''
 API RESPONSE TEST SCRIPT
+
+Speech Recognition Reference: https://github.com/Uberi/speech_recognition/blob/master/examples/microphone_recognition.py
 '''
 from dotenv import load_dotenv
 import os
@@ -8,6 +10,7 @@ import requests
 import translators as ts
 import wolframalpha
 import pyttsx3
+import speech_recognition as sr
 
 load_dotenv()
 
@@ -17,7 +20,6 @@ app_id = os.getenv("APP_ID")
 q_text = "hello, goodbye"
 
 #_ = ts.preaccelerate_and_speedtest() # caching available will work on adding later
-# print(ts.translators_pool) # see available translators
 translation = ts.translate_text(query_text=q_text, translator="google", to_language="ja")
 print(translation)
 
@@ -25,6 +27,21 @@ print(translation)
 #engine = pyttsx3.init()
 #engine.say("hello, world")
 #engine.runAndWait()
+
+
+# speech recog
+r = sr.Recognizer()
+with sr.Microphone() as source:
+		print("Say something")
+ 	audio = r.listen(source)
+
+# Speech Recognition Engine: Sphinx (CMU)
+try:
+	print("What you said: " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+	print("Audio not recognized")
+except sr.RequestError as e:
+	print("Sphinx error; {0}".format(e))
 
 # wolfram test
 client = wolframalpha.Client(app_id)
