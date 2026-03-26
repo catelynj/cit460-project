0d8cc736cdf9e409d9b6e9d4084c6df245a742e2
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-25 21:54:43 -0400

    Ignore

--- touch.py
+++ touch.py
@@ -3,7 +3,6 @@ Touch Sensor Control Script
 
 '''
 import lgpio
-import speech_recognition as sr
 
 TOUCH_PIN = 17
 
@@ -13,17 +12,3 @@ lgpio.gpio_claim_input(h, TOUCH_PIN)
 def read_touch(h, touch_pin):
     return lgpio.gpio_read(h, touch_pin)
 
-def listen():
-  # speech recog
-  r = sr.Recognizer()
-  with sr.Microphone() as source:
-  	print("Say something")
-   	audio = r.listen(source)
-  
-  # Speech Recognition Engine: Sphinx (CMU)
-  try:
-  	print("What you said: " + r.recognize_sphinx(audio))
-  except sr.UnknownValueError:
-  	print("Audio not recognized")
-  except sr.RequestError as e:
-  	print("Sphinx error; {0}".format(e))
