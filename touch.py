08f5df9d7483e2566123618fc14301d8cd6aa7cd
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-25 21:48:40 -0400

    Refactor touch.py

--- touch.py
+++ touch.py
@@ -15,52 +15,15 @@ https://abyz.me.uk/lg/py_lgpio.html
 
 '''
 import lgpio
-
+import speech_recognition as sr
 
 TOUCH_PIN = 17
-touch_count = 0
-TIMEOUT = 0.5
-PRESS_WAIT = 0.4
-last_press = time.time()
 
 h = lgpio.gpiochip_open(0) # init
 lgpio.gpio_claim_input(h, TOUCH_PIN)
 
-last_state = 0
-
-def read_touch():
-  print("awaiting input...")
-  try:
-  	while True:
-  		current_state = lgpio.gpio_read(h, TOUCH_PIN)
-  
-  		if current_state == 1 and last_state == 0:  # rising edge
-  			if time.time() - last_press > TIMEOUT:
-  				touch_count = 0
-  			touch_count += 1
-  			last_press = time.time()
-        
-      if touch_count > 0 and time.time() - last_press > PRESS_WAIT:
-  			if touch_count == 1:
-  				print("prompt")
-  			elif touch_count == 2:
-  				cam.start()
-  				cam.capture_file(p_filename)
-  				print(f"picture taken, saved as {p_filename}")
-  			elif touch_count >= 3:
-  				print("recording 6 second video")
-  				cam.start_recording(encoder, output, quality=Quality.HIGH)
-  				time.sleep(6)
-  				cam.stop_recording()
-  				print(f"recording stopped, saved as {v_filename}")
-  			touch_count = 0
-  
-  		last_state = current_state
-  		time.sleep(0.05)
-  
-  except KeyboardInterrupt:
-  	lgpio.gpiochip_close(h)
-
+def read_touch(h, touch_pin):
+    return lgpio.gpio_read(h, touch_pin)
 
 def listen():
   # speech recog
