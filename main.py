62bc672e33155dd0764dfb6052aecdae32db0286
Catelyn <113139640+catelynj@users.noreply.github.com>
2026-03-27 15:27:15 -0400

    Fixing indent issues

--- main.py
+++ main.py
@@ -18,39 +18,38 @@ TIMEOUT = 0.5
 PRESS_WAIT = 0.4
 last_press = time.time()
 last_state = 0
-
+recording = False 
 
 print("awaiting input...")
 try:
-	while True:
-		current_state = read_touch()
-		
-		if current_state == 1 and last_state == 0:  # rising edge
-			if time.time() - last_press > TIMEOUT:
-				touch_count = 0
-			touch_count += 1
-			last_press = time.time()
-		if touch_count > 0 and time.time() - last_press > PRESS_WAIT:
-			if touch_count == 1:
-				print("prompt")
-			elif touch_count == 2:
-				take_picture()
-			elif touch_count >= 3:
-				start_recording()
-				recording = True
-				touch_count = 0
-			
-			if recording:
-				if touch_count >= 3:
-					stop_recording()
-					recording = False
-					touch_count = 0
+    while True:
+        current_state = read_touch()
 
-			touch_count = 0
-			print("awaiting input...") # reprint for clarity
-		last_state = current_state
-		time.sleep(0.05)
+        if current_state == 1 and last_state == 0:  # rising edge
+            if time.time() - last_press > TIMEOUT:
+                touch_count = 0
+            touch_count += 1
+            last_press = time.time()
+
+        if recording and touch_count >= 3:  # check recording state first, outside tap block
+            stop_recording()
+            recording = False
+            touch_count = 0
+            print("awaiting input...")
+
+        elif touch_count > 0 and time.time() - last_press > PRESS_WAIT:
+            if touch_count == 1:
+                print("prompt")
+            elif touch_count == 2:
+                take_picture()
+            elif touch_count >= 3:
+                start_recording()
+                recording = True
+            touch_count = 0
+            print("awaiting input...")
+
+        last_state = current_state
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
-	lgpio.gpiochip_close(h)
-
+    lgpio.gpiochip_close(h)
